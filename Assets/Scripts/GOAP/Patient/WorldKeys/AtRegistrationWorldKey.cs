a95c6406375e9aa930e10af74d271aa53c292385
ALeving <fgileving@gmail.com>
2025-10-06 19:06:22 +0300

    clean

--- Assets/Scripts/GOAP/Patient/WorldKeys/AtRegistrationWorldKey.cs
+++
@@ -1,7 +0,0 @@
-using CrashKonijn.Goap.Runtime;
-
-namespace GOAP.Patient
-{
-    [GoapId("AtRegistrationWorldKey-09f178e4-3e80-420e-ae96-294d335a4743")]
-    public class AtRegistrationWorldKey : WorldKeyBase {}
-}
